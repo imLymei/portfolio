f01af18d9d042f5bea1b4069bd12eaa780dcb660
Lymei <lymeicontato@gmail.com>
2022-12-08 01:46:38 -0300

    att

--- app/head.jsx
+++ app/head.jsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Lymei | Felipe Cardoso</title>
+      <title>Felipe Cardoso | Lymei : Full Stack Developer Portfolio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/icon.ico" />
       <meta content="Lymei.art" property="og:title"/>
       <meta content="Portfolio of fullstack developer Felipe Cardoso" property="og:description"/>
       <meta content="https://cdn.discordapp.com/attachments/457992163670753301/1050263102446379018/image.png" property="og:image"></meta>
