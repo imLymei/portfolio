c551f14f7bea19ad570fe3ff40069bcb86cd5dc6
Lymei <lymeicontato@gmail.com>
2022-12-08 01:54:28 -0300

    meta changes

--- app/head.jsx
+++ app/head.jsx
@@ -1,12 +1,18 @@
 export default function Head() {
   return (
     <>
-      <title>Felipe Cardoso | Lymei : Full Stack Developer Portfolio</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/icon.ico" />
-      <meta content="Lymei.art" property="og:title"/>
-      <meta content="Portfolio of fullstack developer Felipe Cardoso" property="og:description"/>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <title>Felipe Cardoso | Lymei : Full Stack Developer Portfolio</title>
+      <meta content="Portfolio of fullstack developer Felipe Cardoso with a unique and minimalist website." property="description"/>
+      <meta content="Fullstack Developer | Felipe Cardoso" property="og:title"/>
+      <meta content="Portfolio of fullstack developer Felipe Cardoso with a unique and minimalist website." property="og:description"/>
       <meta content="https://cdn.discordapp.com/attachments/457992163670753301/1050263102446379018/image.png" property="og:image"></meta>
+      <meta content="Fullstack Developer | Felipe Cardoso" property="twitter:title"></meta>
+      <meta content="Portfolio of fullstack developer Felipe Cardoso with a unique and minimalist website." property="twitter:description"></meta>
+      <meta content="https://cdn.discordapp.com/attachments/457992163670753301/1050263102446379018/image.png" property="twitter:image"></meta>
+      <meta content="summary_large_image" name="twitter:card"></meta>
+      <meta property="og:type" content="website"></meta>
     </>
   )
 }
