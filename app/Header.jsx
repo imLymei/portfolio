af8af3c38f1997d639bd7b807ad0ea9d46386f09
Lymei <lymeicontato@gmail.com>
2022-12-08 12:01:05 -0300

    mobile

--- app/Header.jsx
+++ app/Header.jsx
@@ -20,20 +20,20 @@ function Header() {
 
   return (
     <header className='flex justify-center'>
-      <div className='w-full max-w-[85vw] grid grid-cols-3 items-center gap-2 px-0 py-3 sm:p-10 fixed'>
+      <div className='w-full max-w-[90vw] sm:max-w-[85vw] grid grid-cols-3 items-center gap-2 px-0 py-3 sm:p-10 fixed'>
         <div className='relative'>
-        <h1 className='headerText relative left-2 bottom-[-0.4rem]'>Felipe</h1>
-        <h1 className='headerText relative left-[2.7rem] bottom-[0.4rem]'>Cardoso</h1>
+          <h1 className='headerText relative left-2 bottom-[-0.4rem]'>Felipe</h1>
+          <h1 className='headerText relative left-[2.3rem] sm:left-[2.7rem] bottom-[0.4rem]'>Cardoso</h1>
+        </div>
+        <div className='mx-auto items-center'>
+          <a href='#home' style={visible == 1?null:{pointerEvents: 'none'}} className="fixed bottom-5 right-[47vw] sm:static sm:inline">
+            <ArrowUpIcon style={{opacity: `${visible}`}} className='headerIcon transition-opacity ease-in duration-400' />
+         </a>
+        </div>
+        <div className='flex justify-end items-center'>
+          <Bars3BottomRightIcon className='headerIcon cursor-pointer' />
+        </div>
       </div>
-      <div className='mx-auto items-center'>
-        <a href='#home' style={visible == 1?null:{pointerEvents: 'none'}} className="hidden sm:inline">
-          <ArrowUpIcon style={{opacity: `${visible}`}} className='headerIcon transition-opacity ease-in duration-400' />
-        </a>
-      </div>
-      <div className='flex justify-end items-center'>
-        <Bars3BottomRightIcon className='headerIcon cursor-pointer' />
-      </div>
-    </div>
     </header>
   )
 }
