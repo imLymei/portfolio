d22995cc50a8a644eebfcdedc6b46deb8e661bb7
Lymei <lymeicontato@gmail.com>
2022-12-09 00:23:26 -0300

    srollbar

--- app/head.jsx
+++ app/head.jsx
@@ -2,16 +2,21 @@ export default function Head() {
   return (
     <>
       <link rel="icon" href="/icon.ico" />
+
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+
       <title>Felipe Cardoso | Lymei : Full Stack Developer Portfolio</title>
       <meta content="Portfólio de desenvolvedor full stack do programador Felipe Cardoso, com um website unico e minimalista" property="description"/>
+
       <meta content="Full Stack Developer | Felipe Cardoso" property="og:title"/>
       <meta content="Portfólio de desenvolvedor full stack do programador Felipe Cardoso, com um website unico e minimalista" property="og:description"/>
       <meta content="https://cdn.discordapp.com/attachments/457992163670753301/1050263102446379018/image.png" property="og:image"></meta>
+
       <meta content="Full Stack Developer | Felipe Cardoso" property="twitter:title"></meta>
       <meta content="Portfólio de desenvolvedor full stack do programador Felipe Cardoso, com um website unico e minimalista" property="twitter:description"></meta>
       <meta content="https://cdn.discordapp.com/attachments/457992163670753301/1050263102446379018/image.png" property="twitter:image"></meta>
       <meta content="summary_large_image" name="twitter:card"></meta>
+      
       <meta property="og:type" content="website"></meta>
     </>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,26 @@
 import Image from 'next/image'
 
 export default function Home() {
-{/*const navStatus = sessionStorage.getItem('navStatus');*/}
 
   return (
     <div className='bg-noise'>
+
       <div className='h-[100vh] bg-black/95 flex justify-center items-center'>
         <div className='text-5xl text-[#f8f8f8] font-bold'>What</div>
       </div>
+
       <div className='h-[100vh] bg-black/95 flex justify-center items-center'>
         <div className='text-5xl text-[#f8f8f8] font-bold'>Do</div>
       </div>
+
       <div className='h-[100vh] bg-black/95 flex justify-center items-center'>
         <div className='text-5xl text-[#f8f8f8] font-bold'>Whe</div>
       </div>
+      
       <div className='h-[100vh] bg-black/95 flex justify-center items-center'>
         <div className='text-5xl text-[#f8f8f8] font-bold'>Want?</div>
       </div>
+
     </div>
   )
 }
