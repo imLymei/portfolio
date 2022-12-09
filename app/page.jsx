9c5c2f24ed7cf7f7d860c913376ebbb694d6b93e
Lymei <lymeicontato@gmail.com>
2022-12-08 22:17:50 -0300

    UI rework

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,22 @@
 import Image from 'next/image'
 
 export default function Home() {
+{/*const navStatus = sessionStorage.getItem('navStatus');*/}
+
   return (
-    <>
-    <div className='h-[100vh] bg-[#f8f8f8] flex justify-center items-center'>
-      <h1 className='text-5xl text-[#36f1cd] font-bold'>Cultured</h1>
+    <div className='bg-noise'>
+      <div className='h-[100vh] bg-black/95 flex justify-center items-center'>
+        <div className='text-5xl text-[#f8f8f8] font-bold'>What</div>
+      </div>
+      <div className='h-[100vh] bg-black/95 flex justify-center items-center'>
+        <div className='text-5xl text-[#f8f8f8] font-bold'>Do</div>
+      </div>
+      <div className='h-[100vh] bg-black/95 flex justify-center items-center'>
+        <div className='text-5xl text-[#f8f8f8] font-bold'>Whe</div>
+      </div>
+      <div className='h-[100vh] bg-black/95 flex justify-center items-center'>
+        <div className='text-5xl text-[#f8f8f8] font-bold'>Want?</div>
+      </div>
     </div>
-    <div className='h-[100vh] bg-black flex justify-center items-center'>
-      <div className='text-5xl text-[#f8f8f8] font-bold'>Black</div>
-    </div>
-    <div className='h-[100vh] bg-[#037171] flex justify-center items-center'>
-      <div className='text-5xl text-[#f8f8f8] font-bold'>Skolbeloff</div>
-    </div>
-    </>
   )
 }
