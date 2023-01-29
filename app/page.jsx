a2da6702c792f74d25df11555338745c9ff8499f
Lymei <ronaldofelipespm@bol.com.br>
2023-01-29 16:56:43 -0300

    Animations update

--- app/page.jsx
+++ app/page.jsx
@@ -7,15 +7,17 @@ import { BsFillMoonStarsFill } from 'react-icons/bs';
 import { GiRingedPlanet } from 'react-icons/gi';
 import { IoLogoCss3, IoLogoJavascript, IoLogoPython } from 'react-icons/io';
 import { SiFigma, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss } from 'react-icons/si';
-import Astronaut2 from '../public/astronaut2.webp';
 import Astronaut from '../public/astronaut.png';
 
 export default function Home() {
 	const [darkMode, setDarkMode] = useState(true);
 
 	return (
-		<div className={darkMode ? 'star text-white  dark' : ' bg-white '}>
+		<div className={darkMode ? 'whiteStar text-white  dark transition-all' : ' blackStar transition-all'}>
 			<main className='px-10'>
+				<p className='absolute text-xs sm:right-[4vw] right-[16vw] sm:top-[9vh] top-[10vh] floating dark:opacity-0 opacity-50'>
+					White Space...
+				</p>
 				<section className='min-h-screen'>
 					<nav className='py-10 mb-12 flex justify-between'>
 						<GiRingedPlanet className='text-4xl dark:text-white floating' />
@@ -80,7 +82,7 @@ export default function Home() {
 				<section className='relative text-center min-h-screen'>
 					<h2 className='text-3xl py-16 floating'>Skill Set</h2>
 					<div className='grid grid-cols-1 x820:grid-cols-2 sm:p-16 gap-8 sm:gap-4'>
-						<div className='dark:bg-black/75 shadow-lg dark:shadow-white/10 p-2 m-auto flex flex-col justify-center items-center min-w-[300px] w-[30vw] max-w-[70vw] min-h-[144px] h-[20vh] border border-black dark:border-white rounded-sm floating'>
+						<div className='dark:bg-black/75 bg-white/75 shadow-lg dark:shadow-white/10 p-2 m-auto flex flex-col justify-center items-center min-w-[300px] w-[30vw] max-w-[70vw] min-h-[144px] h-[20vh] border border-black dark:border-white rounded-sm floating'>
 							<h3 className='text-2xl py-2'>Linguagens</h3>
 							<div className='text-4xl md:text-5xl flex justify-center gap-[6%]'>
 								<a
@@ -103,7 +105,7 @@ export default function Home() {
 								</a>
 							</div>
 						</div>
-						<div className='dark:bg-black/75 shadow-lg dark:shadow-white/10 p-2 m-auto flex flex-col justify-center items-center min-w-[300px] w-[30vw] max-w-[70vw] min-h-[144px] h-[20vh] border dark:border-white border-black rounded-sm floating-1'>
+						<div className='dark:bg-black/75 bg-white/75 shadow-lg dark:shadow-white/10 p-2 m-auto flex flex-col justify-center items-center min-w-[300px] w-[30vw] max-w-[70vw] min-h-[144px] h-[20vh] border dark:border-white border-black rounded-sm floating-1'>
 							<h3 className='text-2xl py-2'>Frameworks/Library</h3>
 							<div className='text-4xl md:text-5xl flex justify-center gap-[3%]'>
 								<a href='https://reactjs.org' target='_blank' rel='noreferrer'>
@@ -219,7 +221,7 @@ export default function Home() {
 					</div>
 				</section>
 			</main>
-			<footer className='p-8 px-[10vw] x820:px-[25vw] text-center border-t dark:bg-black border-black dark:border-white grid grid-cols-2'>
+			<footer className='p-8 px-[10vw] x820:px-[25vw] text-center border-t dark:bg-black bg-white border-black dark:border-white grid grid-cols-2'>
 				<h2 className='text-sm dark:text-white'>Felipe Cardoso©2022</h2>
 				<h2 className='text-sm dark:text-white'>
 					Powered by <a href=''>Next.js</a>
