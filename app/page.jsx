6f4d3efc99e72ae63e8f9707c3bf3df6f7662333
Lymei <ronaldofelipespm@bol.com.br>
2023-02-03 14:16:06 -0300

    Css update

--- app/page.jsx
+++ app/page.jsx
@@ -45,13 +45,9 @@ export default function Home() {
 					<div className='text-center py-10 sm:p-10'>
 						<h1 className='text-7xl sm:text-8xl py-2 floating-2'>FELIPE CARDOSO</h1>
 						<h2 className='text-3xl py-2 floating-1'>Fullstack developer e programador</h2>
-						<p className='py-6 leading-8 sm:px-16 lg:px-40'>
+						<p className='py-6 pText'>
 							Estudante de programação na faculdade{' '}
-							<a
-								href='https://www.unisanta.br'
-								target='_blank'
-								rel='noreferrer'
-								className='text-indigo-700 dark:text-indigo-500'>
+							<a href='https://www.unisanta.br' target='_blank' rel='noreferrer' className='link'>
 								Universidade Santa Cecília
 							</a>{' '}
 							do curso{' '}
@@ -59,7 +55,7 @@ export default function Home() {
 								href='https://conteudo.unisanta.br/landing-page-tecnologo-analise-e-desenvolvimento-de-sistemas '
 								target='_blank'
 								rel='noreferrer'
-								className='text-indigo-700 dark:text-indigo-500'>
+								className='link'>
 								Análise e Desenvolvimento de Sistemas
 							</a>
 							, com foco em <span className='text-pink-500'>desenvolvimento web e de software</span>. Estou em
@@ -69,10 +65,10 @@ export default function Home() {
 					</div>
 					<div className='text-5xl flex justify-center gap-16 py-6'>
 						<a href='https://www.linkedin.com/in/imlymei/' rel='noreferrer' target={'_blank'}>
-							<AiFillLinkedin className='sm:hover:scale-110 transition-all floating-1' />
+							<AiFillLinkedin className='hover110 floating-1' />
 						</a>
 						<a href='https://github.com/imLymei' rel='noreferrer' target={'_blank'}>
-							<AiFillGithub className='sm:hover:scale-110 transition-all floating' />
+							<AiFillGithub className='hover110 floating' />
 						</a>
 					</div>
 					<div className='relative left-[-20%] sm:left-[-10%] h-[30vw] sm:h-[20vw] w-[60vw] sm:w-[45vw] my-10 astronaut'>
@@ -82,60 +78,56 @@ export default function Home() {
 				<section className='relative text-center min-h-screen'>
 					<h2 className='text-3xl py-16 floating'>Skill Set</h2>
 					<div className='grid grid-cols-1 x820:grid-cols-2 sm:p-16 gap-8 sm:gap-4'>
-						<div className='dark:bg-black/75 bg-white/75 shadow-lg dark:shadow-white/10 p-2 m-auto flex flex-col justify-center items-center min-w-[300px] w-[30vw] max-w-[70vw] min-h-[144px] h-[20vh] border border-black dark:border-white rounded-sm floating'>
+						<div className='skillCard floating'>
 							<h3 className='text-2xl py-2'>Linguagens</h3>
 							<div className='text-4xl md:text-5xl flex justify-center gap-[6%]'>
 								<a
 									href='https://developer.mozilla.org/en-US/docs/Glossary/HTML5'
 									target='_blank'
 									rel='noreferrer'>
-									<AiFillHtml5 className='sm:hover:scale-110 transition-all' />
+									<AiFillHtml5 className='hover110' />
 								</a>
 								<a href='https://developer.mozilla.org/pt-BR/docs/Web/CSS' target='_blank' rel='noreferrer'>
-									<IoLogoCss3 className='sm:hover:scale-110 transition-all' />
+									<IoLogoCss3 className='hover110' />
 								</a>
 								<a
 									href='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript'
 									target='_blank'
 									rel='noreferrer'>
-									<IoLogoJavascript className='sm:hover:scale-110 transition-all' />
+									<IoLogoJavascript className='hover110' />
 								</a>
 								<a href='https://www.python.org' target='_blank' rel='noreferrer'>
-									<IoLogoPython className='sm:hover:scale-110 transition-all' />
+									<IoLogoPython className='hover110' />
 								</a>
 							</div>
 						</div>
-						<div className='dark:bg-black/75 bg-white/75 shadow-lg dark:shadow-white/10 p-2 m-auto flex flex-col justify-center items-center min-w-[300px] w-[30vw] max-w-[70vw] min-h-[144px] h-[20vh] border dark:border-white border-black rounded-sm floating-1'>
+						<div className='skillCard floating-1'>
 							<h3 className='text-2xl py-2'>Frameworks/Library</h3>
 							<div className='text-4xl md:text-5xl flex justify-center gap-[3%]'>
 								<a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-									<SiReact className='sm:hover:scale-110 transition-all' />
+									<SiReact className='hover110' />
 								</a>
 								<a href='https://nextjs.org' target='_blank' rel='noreferrer'>
-									<SiNextdotjs className='sm:hover:scale-110 transition-all' />
+									<SiNextdotjs className='hover110' />
 								</a>
 								<a href='https://tailwindcss.com' target='_blank' rel='noreferrer'>
-									<SiTailwindcss className='sm:hover:scale-110 transition-all' />
+									<SiTailwindcss className='hover110' />
 								</a>
 								<a href='https://nodejs.org/en/' target='_blank' rel='noreferrer'>
-									<SiNodedotjs className='sm:hover:scale-110 transition-all' />
+									<SiNodedotjs className='hover110' />
 								</a>
 								<a href='https://www.figma.com' target='_blank' rel='noreferrer'>
-									<SiFigma className='sm:hover:scale-110 transition-all' />
+									<SiFigma className='hover110' />
 								</a>
 							</div>
 						</div>
 					</div>
 					<div className='py-16'>
 						<h2 className='text-3xl mt-8 floating-1'>Portfolio</h2>
-						<p className='leading-8 sm:px-16 lg:px-40 py-8 p-6'>
+						<p className='pText py-8 p-6'>
 							Aqui estão alguns dos meus <span className='text-pink-500'>mais recentes projetos</span>{' '}
 							relacionados principalmente com a criação de sites usando{' '}
-							<a
-								href='https://nextjs.org'
-								target='_blank'
-								rel='noreferrer'
-								className='text-indigo-700 dark:text-indigo-500'>
+							<a href='https://nextjs.org' target='_blank' rel='noreferrer' className='link'>
 								Next.js
 							</a>{' '}
 							ou alguns programas utilizando{' '}
@@ -143,29 +135,21 @@ export default function Home() {
 								href='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript'
 								target='_blank'
 								rel='noreferrer'
-								className='text-indigo-700 dark:text-indigo-500'>
+								className='link'>
 								Javascript
 							</a>{' '}
 							ou{' '}
-							<a
-								href='https://www.python.org'
-								target='_blank'
-								rel='noreferrer'
-								className='text-indigo-700 dark:text-indigo-500'>
+							<a href='https://www.python.org' target='_blank' rel='noreferrer' className='link'>
 								Python
 							</a>
 							. Acesse meu{' '}
-							<a
-								href='https://github.com/imLymei'
-								target='_blank'
-								rel='noreferrer'
-								className='text-indigo-700 dark:text-indigo-500'>
+							<a href='https://github.com/imLymei' target='_blank' rel='noreferrer' className='link'>
 								Github
 							</a>{' '}
 							caso queira ver <span className='text-pink-500'>todos os meus projetos</span>.
 						</p>
 						<div className='grid grid-cols-2 md:grid-cols-4 justify-items-center gap-4'>
-							<div className='relative min-w-[144px] min-h-[81px] w-[20vw] aspect-video dark:bg-white bg-black sm:hover:scale-110 transition-all rounded-sm border border-black dark:border-white floating-1'>
+							<div className='portfolioCard floating-1'>
 								<a
 									className='cursor-pointer'
 									href='https://captur-one.vercel.app'
@@ -178,7 +162,7 @@ export default function Home() {
 									/>
 								</a>
 							</div>
-							<div className='relative min-w-[144px] min-h-[81px] [20vw] aspect-video dark:bg-white bg-black sm:hover:scale-110 transition-all rounded-sm border border-black dark:border-white floating-2'>
+							<div className='portfolioCard floating-2'>
 								<a
 									className='cursor-pointer'
 									href='https://physic-engine.vercel.app'
@@ -191,7 +175,7 @@ export default function Home() {
 									/>
 								</a>
 							</div>
-							<div className='relative min-w-[144px] min-h-[81px] [20vw] aspect-video dark:bg-white bg-black sm:hover:scale-110 transition-all rounded-sm border border-black dark:border-white floating'>
+							<div className='portfolioCard floating'>
 								<a
 									className='cursor-pointer'
 									href='https://tower-of-hanoi-eight.vercel.app'
@@ -204,7 +188,7 @@ export default function Home() {
 									/>
 								</a>
 							</div>
-							<div className='relative min-w-[144px] min-h-[81px] [20vw] aspect-video dark:bg-white bg-black sm:hover:scale-110 transition-all rounded-sm border border-black dark:border-white floating-1'>
+							<div className='portfolioCard floating-1'>
 								<a
 									className='cursor-pointer'
 									href='https://self-driving-car-ten.vercel.app'
